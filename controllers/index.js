30d4e22f7e80cdd96757a696849bb38f300265d2
ranimje <ranimje@paypal.com>
2015-09-19 16:10:02 -0700

    Added changes for Register and routing user to use hyperlinks
    Added changes for Register and routing user to use hyperlinks

--- controllers/index.js
+++ controllers/index.js
@@ -9,28 +9,40 @@ module.exports = function (router) {
     var model = new IndexModel();
 
     router.get('/', function (req, res) {
-        
-       surveryData('start.json',function(err,survey){
-           console.log(JSON.stringify(survey));
-           res.render('index', survey);
-       });
 
+        surveryData('newAccount.json',function(err,survey){
+                  console.log(JSON.stringify(survey));
+                    res.render(survey.templateName, survey);
+                });
+             });
+        
+   //    surveryData('currentBusinessStatus.json',function(err,survey){
+   //        console.log(JSON.stringify(survey));
+   //        res.render(survey.templateName, survey);
+   //    });
+   // });
 
-        
-        
+    router.get('/createAccount', function (req, res) {
+        //TODD: Save account information to database if action is create
+        console.log("I am here");
+        console.log(req.query.nextRoute);
+        console.log((req));
+        surveryData(req.query.nextRoute,function(err,survey){
+            console.log(req.params);
+            console.log(JSON.stringify(survey));
+            res.render(survey.templateName, survey);
+        });
     });
+
 
     router.get('/nextSurvey', function (req, res) {
         console.log((req));
         surveryData(req.query.nextRoute,function(err,survey){
             console.log(req.params);
             console.log(JSON.stringify(survey));
-            res.render('index', survey);
+            res.render(survey.templateName, survey);
         });
-
-
-
-
     });
+
 
 };
