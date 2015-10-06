3b8dee623a450ebe26de8023b88322e4f858cea8
Patwardhan <tpatwardhan@ebay.com>
2015-10-06 15:25:26 -0700

    UI enhancements

--- .build/js/w2f.js
+++ .build/js/w2f.js
@@ -15,14 +15,26 @@ $(document).ready(function () {
 
 	$(document).on('click','.response',function(){
 
-console.log("called called");
+
 		console.log(this.value);
+		var that = $(this);
 
 		$.ajax({
 			url:"/nextSurvey?nextRoute="+ this.value,
 			success:function(result){
-				//alert("success");
+				console.log(that);
+				that.prop("disabled",true);
+				that.parent().siblings().children('.response').prop("disabled",true);
+				that.parent().siblings().children('.response').removeClass('btn-primary').addClass('btn-default').css("background-color","grey");
+
+if(that.data("id")=="yes") {
+	console.log(that.parent().siblings("div.resultArrow"));
+	that.closest(".questionTemplate").siblings("div.resultArrow").html('<div class=" col-md-offset-1"><span class="arrow-success-large" data-angle="180" style="transform: rotate(180deg);"></span></div>');
+}else{
+	that.closest(".questionTemplate").siblings("div.resultArrow").html('<div class=" col-md-offset-3"><span class="arrow-danger-large" data-angle="180" style="transform: rotate(180deg);"></span></div>');
+}
 				$("#questionHolder").append(result);
+			//	$('.downarrows').css('display', 'block');
 			},
 			error:function(result){
 
